757a1d16a00d03bcfc6ce9dea287f433f53075f1
Yurij Finiv <Yura.Finiv@gmail.com>
2026-09-24 20:48:36 +0300

    fix: normalize XML-prefixed SVG menu icons

--- resources/views/menu-label.blade.php
+++ resources/views/menu-label.blade.php
@@ -10,7 +10,19 @@
         }
     }
 
-    $rawSvgIcon = is_string($icon) && str_starts_with(ltrim($icon), '<svg');
+    $svgIcon = null;
+
+    if (is_string($icon)) {
+        $normalizedIcon = ltrim($icon, "\xEF\xBB\xBF \t\n\r\0\x0B");
+        $normalizedIcon = preg_replace('/^<\?xml[^?]*\?>\s*/i', '', $normalizedIcon) ?? $normalizedIcon;
+        $normalizedIcon = preg_replace('/^<!DOCTYPE[^>]*>\s*/i', '', $normalizedIcon) ?? $normalizedIcon;
+
+        if (preg_match('/^<svg\b/i', $normalizedIcon) === 1) {
+            $svgIcon = $normalizedIcon;
+        }
+    }
+
+    $rawSvgIcon = $svgIcon !== null;
 @endphp
 
 @if(is_string($iconView) && view()->exists($iconView))
@@ -19,7 +31,7 @@
     </span>
 @elseif($rawSvgIcon)
     <span class="inline-flex h-5 w-5 shrink-0 items-center justify-center [&>svg]:h-5 [&>svg]:w-5" aria-hidden="true">
-        {!! $icon !!}
+        {!! $svgIcon !!}
     </span>
 @elseif(filled($icon))
     <i class="{{ $icon }} shrink-0" aria-hidden="true"></i>
